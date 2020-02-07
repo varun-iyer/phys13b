11a4ee198bd74e066893e98ca4ba220a0653da51
Varun Iyer <varun_iyer@posteo.net>
2020-02-07 15:39:30 -0800

    stuff

--- iyer_3071560_hw4/hw4p1b_plot.py
+++ iyer_3071560_hw4/hw4p1b_plot.py
@@ -1,8 +1,19 @@
+import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
-dacv, pin1, vout = np.load("hw4p1b.npy")
-i_res = (pin1 - vout) / 98.0
+try:
+    dacv, pin1, vout = np.loadtxt(sys.argv[1])
+except IndexError:
+    print("usage: python3 hw4p1b_plot.py /path/to/data.txt /path/to/save.eps")
 
+i_res = (pin1 - vout) / 98.0
 plt.plot(vout, i_res)
-plt.show()
+plt.xlabel("Voltage Across LED (V)")
+plt.ylabel("Current Through LED (A)")
+ 
+try:
+    plt.savefig(sys.argv[2], format="eps")
+    print("Plot saved to {}.".format(sys.argv[2]))
+except IndexError:
+    print("usage: python3 hw4p1b_plot.py /path/to/data.txt /path/to/save.eps")
